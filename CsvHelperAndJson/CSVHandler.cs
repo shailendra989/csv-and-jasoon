48eeeab810f9373323ed471c7aac226f5c11982b
Shailendra kumar pal <palshailendrakumar123@gmail.com>
2023-02-20 21:57:49 +0530

    first commit

---
+++ CsvHelperAndJson/CSVHandler.cs
@@ -0,0 +1,55 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvHelperAndJson
+{
+
+
+    class CSVHandler
+    {
+        public static void ImplementCSVDataHandling()
+        {
+            string importFilePath = "C:\\Users\\91790\\Desktop\\dotnet\\CsvHelperAndJson\\CsvHelperAndJson\\Utility\\Addresses.csv";
+            string exportFilePath = "C:\\Users\\91790\\Desktop\\dotnet\\CsvHelperAndJson\\CsvHelperAndJson\\Utility\\export.csv";
+
+            //reading csv file
+            using (var reader = new StreamReader(importFilePath))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<AddressData>().ToList();
+                Console.WriteLine("Read data successfully from addresses csv.");
+                foreach (AddressData addressData in records)
+                {
+                    Console.Write("\t" + addressData.firstname);
+                    Console.Write("\t" + addressData.lastname);
+                    Console.Write("\t" + addressData.address);
+                    Console.Write("\t" + addressData.city);
+                    Console.Write("\t" + addressData.state);
+                    Console.Write("\t" + addressData.code);
+                    Console.WriteLine();
+
+                }
+                Console.WriteLine("**********************Reading fromcsv file and Write to csv file **************************");
+                //Writing csv file
+
+                using (var writer = new StreamWriter(exportFilePath))
+                using (var csvExport = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    csvExport.WriteRecords(records);
+                }
+            }
+            //Console.WriteLine(reader);
+        }
+
+
+    }
+
+
+
+}
---
+++ CsvHelperAndJson/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace CsvHelperAndJson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         //   Console.WriteLine("Hello, World!");
+         //   Console.WriteLine("# Read data from CSV & Write data in CSV");
+          //  CSVHandler.ImplementCSVDataHandling();
+           ReadCSV_And_WriteJSON.ImplementCSVToJSON();
+
+           // Console.ReadKey();
+           
+        }
+    }
+}
